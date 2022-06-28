da56f84e1b7dafded5156c70057a4bbf2b85fcdc
Elaine1066 <93468011+Elaine1066@users.noreply.github.com>
2022-06-28 17:19:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,339 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo "Hello Azure!";
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Bahtera Ferry Services</title>
+    <link rel="icon" href="img/core-img/logo2.jpg">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="cssload-container">
+            <div class="cssload-loading"><i></i><i></i><i></i><i></i></div>
+        </div>
+    </div>
+
+    <header class="header-area">
+        <div class="bahtera-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <nav class="classy-navbar justify-content-between" id="bahteraNav">
+
+                        <a href="index.php" class="nav-brand"><img src="img/core-img/logo.png" alt="" style="width: 200px; height: 80px;"></a>
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+                        <div class="classy-menu">
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <div class="classynav">
+                                <ul><li class="active"><a href="index.php">Home</a></li>
+                                    <li><a href="about-us.php">About Us</a></li>
+                                    <li><a href="schedule.php">Schedule</a></li>
+                                    <li><a href="contact.php">Contact Us</a></li>   
+                                    <li><a href="login.php">Login/Sign Up</a></li>                                
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="hero-area">
+        <div class="hero-slides owl-carousel">
+
+            <div class="single-hero-slide d-flex align-items-center justify-content-center">
+                <div class="slide-img bg-img" style="background-image: url(img/bg-img/bg1.jpg);"></div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-lg-9">
+                            <div class="hero-slides-content" data-animation="fadeInUp" data-delay="100ms">
+                                <div class="line" data-animation="fadeInUp" data-delay="300ms"></div>
+                                <h2 data-animation="fadeInUp" data-delay="500ms">&nbsp;The Vacation Heaven&nbsp;</h2>
+                                <p data-animation="fadeInUp" data-delay="700ms">There is nothing more enticing, disenchanting, and enslaving than the life at sea.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="single-hero-slide d-flex align-items-center justify-content-center">
+                <div class="slide-img bg-img" style="background-image: url(img/bg-img/bg2.jpg);"></div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-lg-9">
+                            <div class="hero-slides-content" data-animation="fadeInUp" data-delay="100ms">
+                                <div class="line" data-animation="fadeInUp" data-delay="300ms"></div>
+                                <h2 data-animation="fadeInUp" data-delay="500ms">A journey to remember</h2>
+                                <p data-animation="fadeInUp" data-delay="700ms">Bon voyage!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="single-hero-slide d-flex align-items-center justify-content-center">
+                <div class="slide-img bg-img" style="background-image: url(img/bg-img/bg3.jpg);"></div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-lg-9">
+                            <div class="hero-slides-content" data-animation="fadeInUp" data-delay="100ms">
+                                <div class="line" data-animation="fadeInUp" data-delay="300ms"></div>
+                                <h2 data-animation="fadeInUp" data-delay="500ms">&nbsp;&nbsp;Enjoy your journey&nbsp;&nbsp;</h2>
+                                <p data-animation="fadeInUp" data-delay="700ms">with Bahtera Ferry Services Sdn Bhd.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <div class="book-now-area">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-10">
+                    <div class="book-now-form">
+                        <form action="#">
+                            <div class="form-group">
+                                <label for="select1">Depart Date</label>
+                                <select class="form-control" id="select1">
+                                  <option>14 June</option>
+                                  <option>15 June</option>
+                                  <option>16 June</option>
+                                  <option>17 June</option>
+                                  <option>18 June</option>
+                                  <option>19 June</option>
+                                  <option>20 June</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="select2">Return Date</label>
+                                <select class="form-control" id="select2">
+                                  <option>14 June</option>
+                                  <option>15 June</option>
+                                  <option>16 June</option>
+                                  <option>17 June</option>
+                                  <option>18 June</option>
+                                  <option>19 June</option>
+                                  <option>20 June</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="select3">Pax</label>
+                                <select class="form-control" id="select4">
+                                  <option>1</option>
+                                  <option>2</option>
+                                  <option>3</option>
+                                  <option>4</option>
+                                  <option>5</option>
+                                </select>
+                            </div>
+                            
+                            <button type="submit">Book Now</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="about-us-area">
+        <div class="container">
+            <div class="row align-items-center">
+
+                <div class="col-12 col-lg-6">
+                    <div class="about-text text-center mb-100">
+                        <div class="section-heading text-center">
+                            <div class="line-"></div>
+                            <h2>A journey to remember</h2>
+                        </div>
+                        <p>One of the largest islands off the East coast of Malaysia, the Redang Redang Island is a popular tourist destination. It is a part of the Redang archipelago (a chain of islands scattered in the lakes, rivers or the ocean). The white sandy beaches of Redang slowly lead you towards the crystal clear waters. A few meters further, the sights of tropical fish and coral reefs will delight you! Laze around in the beaches around, walk on the wooden bridge and the sand, relax and eat in the resort, trek and wander in the jungle (but safely with all necessary supplies), dance or sit among the night life, wash yourself with the sea, go snorkelling and diving…</p>
+                        <div class="about-key-text">
+                            <h6><span class="fa fa-check"></span> Crystal clear water</h6>
+                            <h6><span class="fa fa-check"></span> Star rated accomodation</h6>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-lg-6">
+                    <div class="about-thumbnail homepage mb-100">
+                        <div class="first-img wow fadeInUp" data-wow-delay="100ms">
+                            <img src="img/bg-img/5.jpg" alt="">
+                        </div>
+                        <div class="second-img wow fadeInUp" data-wow-delay="300ms">
+                            <img src="img/bg-img/6.jpg" alt="">
+                        </div>
+                        <div class="third-img wow fadeInUp" data-wow-delay="500ms">
+                            <img src="img/bg-img/7.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="vessel-area section-padding-100 bg-img bg-fixed" style="background-image: url(img/bg-img/bg4.jpeg);">
+        <div class="container">
+            <div class="row justify-content-end">
+                <div class="col-12 col-lg-7">
+                    <div class="vessel-content text-center wow fadeInUp" data-wow-delay="300ms">
+                        <div class="section-heading text-center white">
+                            <div class="line-"></div>
+                            <h2>Why Us</h2>
+                            <p>Bahtera Ferry Services Sdn Bhd has the most uncomplicated and hassle-free booking experience ever. Choose your destination, view the seat layout, choose convenient seats, and book your ticket in just a few clicks! Our experience does not end with the booking. We have a dedicated customer-care team to cater for your needs while you are on-the-go. We seek regular feedback from our customers and are always striving to better ourselves!</p>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12 col-sm-4">
+                                <div class="vessel-feature">
+                                    <img src="https://img.icons8.com/clouds/100/undefined/water-transportation.png"/>
+                                    <p>Luxuries Vessel</p>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-4">
+                                <div class="vessel-feature">
+                                    <img src="https://img.icons8.com/external-flaticons-flat-flat-icons/100/undefined/external-captain-vacation-planning-cruise-flaticons-flat-flat-icons.png"/>
+                                    <p>Good Captain</p>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-4">
+                                <div class="vessel-feature">
+                                    <img src="https://img.icons8.com/plasticine/100/undefined/check-all--v1.png"/>
+                                    <p>Safety</p>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#" class="btn bahtera-btn mt-50">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="destinations-area section-padding-100-0">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-6">
+                    <div class="section-heading text-center">
+                        <div class="line-"></div>
+                        <h2>Our Destinations</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-destinations-area wow fadeInUp" data-wow-delay="300ms">
+                        <div class="bg-thumbnail bg-img" style="background-image: url(img/bg-img/bg6.jpeg);"></div>
+                        <p class="price-from">RM 55 / Pax</p>
+                        <div class="destinations-text">
+                            <div class="line"></div>
+                            <h4>Kuala Terengganu</h4>
+                            <p><img src="https://img.icons8.com/arcade/30/undefined/experimental-marker-arcade.png"/>Kuala Terengganu, Terengganu</p>
+                        </div>
+                        <a href="#" class="book-destinations-btn btn bahtera-btn">Book Now</a>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="single-destinations-area wow fadeInUp" data-wow-delay="300ms">
+                        <div class="bg-thumbnail bg-img" style="background-image: url(img/bg-img/bg7.jpeg);"></div>
+                        <p class="price-from">RM 55 / Pax</p>
+                        <div class="destinations-text">
+                            <div class="line"></div>
+                            <h4>Pulau Redang</h4>
+                            <p><img src="https://img.icons8.com/arcade/30/undefined/experimental-marker-arcade.png"/>Kuala Nerus District, Terengganu</p>
+                        </div>
+                        <a href="#" class="book-destinations-btn btn bahtera-btn">Book Now</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="contact-area d-flex flex-wrap align-items-center">
+        <div class="home-map-area">
+            <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Jeti%20Pelancongan%20Shahbandar,Jalan%20Sultan%20Zainal%20Abidin,%2020000%20Kuala%20Terengganu,Terengganu%20Darul%20Iman.+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" allowfullscreen></iframe>
+        </div>
+
+        <div class="contact-info">
+            <div class="section-heading wow fadeInUp" data-wow-delay="100ms">
+                <div class="line-"></div>
+                <h2>Contact Info</h2>
+                <p>
+                    BAHTERA FERRY SERVICES SDN.BHD
+                    <br>
+                    芭特拉渡轮服务有限公司 (1262601-T)
+                </p>
+            </div>
+            <h4 class="wow fadeInUp" data-wow-delay="300ms">Jeti Pelancongan Shahbandar,Jalan Sultan Zainal Abidin,
+20000 Kuala Terengganu,Terengganu Darul Iman.</h4>
+            <h5 class="wow fadeInUp" data-wow-delay="400ms">(+60) 12-345 0127</h5>
+            <h5 class="wow fadeInUp" data-wow-delay="500ms"><a href="mailto:bahteraferry2018@gmail.com" target="_blank" style="font-size: 0.83em;">bahteraferry2018@gmail.com</a></h5>
+        </div>
+    </section>
+
+    <footer class="footer-area">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-12 col-lg-5">
+                    <div class="footer-widget-area mt-50">
+                        <a href="#" class="d-block mb-5"><img src="img/core-img/logo.png" alt=""></a>
+                        <p class="text-justify">BAHTERA FERRY SERVICES SDN.BHD, A ferry company in Terengganu, was founded in 2018 in Kuala Terengganu. We are operating our ferry services from mainland Kuala Terengganu to Kapas Island and Redang Island. We are currently expand our business to ferry terminal, island tour, resort and hotel.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="footer-widget-area mt-50">
+                        <h6 class="widget-title mb-5">Find us on the map</h6>
+                        <img src="img/bg-img/footer-map.png" alt="">
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="footer-widget-area mt-50">
+                        <h6 class="widget-title mb-5">Subscribe to our newsletter</h6>
+                        <form action="#" method="post" class="subscribe-form">
+                            <input type="email" name="subscribe-email" id="subscribeemail" placeholder="Your E-mail">
+                            <button type="submit">Subscribe</button>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <div class="copywrite-text mt-30">
+                        <p>
+                            <a href="#">
+                                Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Bahtera Ferry Services Sdn Bhd
+                            </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <script src="js/bootstrap/popper.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/plugins/plugins.js"></script>
+    <script src="js/active.js"></script>
+</body>
+
+</html>
